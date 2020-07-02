c4ee2ab5ea17b59c0a6091372db24ed4210c2315
ycw3320 <ycw3320@naver.com>
2020-07-02 22:58:26 +0900

    font-aply-done

--- client/src/Component/board/BoardWritePage.jsx
+++ client/src/Component/board/BoardWritePage.jsx
@@ -4,6 +4,7 @@ import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import {NavLink} from "react-router-dom";
+import Input from "reactstrap/es/Input";
 
 function BoardWritePage(props) {
 
@@ -17,13 +18,20 @@ function BoardWritePage(props) {
         setBoardContent(event.currentTarget.value)
     }
 
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+        let body = {
+
+        }
+
+    }
 
     return (
         <div>
             <Container style={Board_style}>
-                <Typography variant="h6" style={{textAlign: "center"}}>자유게시판 글 쓰기</Typography>
+                <Typography variant="h5" style={{textAlign: "center"}}>자유게시판 글 쓰기</Typography>
 
-                <form style={{height: "96%"}}>
+                <form style={{height: "96%"}} onSubmit={}>
                     <TextField variant="filled" label="제목" type="text" placeholder="글의 제목을 입력하세요."
                                fullWidth margin="normal" value={BoardName} onChange={onBoardNameHandler}/>
 
@@ -39,7 +47,7 @@ function BoardWritePage(props) {
                         onChange={onBoardContentHandler}
                     />
 
-
+                    <Input type="file" style={{margin: 5, textDecoration: "none"}}></Input> <br/>
                     <Button variant="contained" type="submit" color="inherit" style={{margin: 5}}>글쓰기 </Button>
                     <Button variant="contained" type="button" color="inherit"
                             style={{margin: 5, textDecoration: "none"}}> 뒤로가기 </Button>
