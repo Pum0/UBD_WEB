afc7b6294a660d5451f899f2478611ecf92c0570
Jin <cjy1521@naver.com>
2020-07-07 10:17:10 +0900

    post number add

--- server/models/Post.js
+++ server/models/Post.js
@@ -2,10 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const moment = require('moment');
 
+const autoIncrement = require('mongoose-auto-increment');
+const connection = mongoose.createConnection("mongodb+srv://Jin:1q2w3e4r@ubd-higbl.mongodb.net/ubd?retryWrites=true&w=majority")
+
+autoIncrement.initialize(connection,{ useNewUrlParser: true });
+
 const postSchema = mongoose.Schema({
     writer: {
         type: Schema.Types.ObjectId,
         ref: 'User'
+    },
+
+    seq: {
+        type: Number
     },
 
     title: {
@@ -40,6 +49,13 @@ const postSchema = mongoose.Schema({
     }
 
 
+})
+
+postSchema.plugin(autoIncrement.plugin, {
+    model: 'postSchemaModel',
+    field: 'seq',
+    startAt: 1,
+    increment: 1
 })
 
 const Post = mongoose.model('Post', postSchema)
